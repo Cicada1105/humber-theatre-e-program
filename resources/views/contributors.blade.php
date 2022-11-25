8b9cae6471e55c9edbedfde1baeafd8a85db0ab0
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro-2.local>
2022-11-24 22:21:51 -0500

    Completed the contributors page, using an enumeration for the department types to aid in type safety and for styling.

--- resources/views/contributors.blade.php
+++ resources/views/contributors.blade.php
@@ -1,5 +1,26 @@
 @extends('layout.page_template')
 
+@section('styles')
+	<link rel="stylesheet" href="/css/contributors.css" />
+@endsection
+
 @section('main-content')
-	<h1>{{ $message }}</h1>
+	<h1>{{ $program }}: Contributors</h1>
+	<hgroup class="departments flex-container m-auto">
+		@foreach($departments as $department)
+			<h3 style="color: {{ $department->color() }}">{{ ucwords($department->value) }}</h3>
+		@endforeach
+	</hgroup>
+	<section class="contributors-section">
+		@foreach($contributors as $contributor)
+			<article class="contributor">
+				<h4 class="contributor__name">{{ $contributor['name'] }}</h4>
+				<hgroup class="role flex-container">
+					<h5 style="color: {{ $contributor['department']->color() }}">{{ ucwords($contributor['department']->value) }}</h5>
+					<h5>{{ $contributor['role'] }}</h5>
+				</hgroup>
+				<p class="contributor__bio">{{ $contributor['bio'] }}</p>
+			</article>
+		@endforeach
+	</section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 define('PROGRAMS', ['the-humans', 'program-2', 'program-3']);
 define('ADDITIONAL_PAGES', ['humber-theatre']);
 
+enum DepartmentTypes:string {
+    case Performance = "performance";
+    case GuestArtist = "guest artist";
+    case Production = "production";
+
+    public function color(): string {
+        return match($this) {
+            DepartmentTypes::Performance => '#0C87CE',
+            DepartmentTypes::GuestArtist => '#EA7994',
+            DepartmentTypes::Production => '#3FA276'
+        };
+    }
+}
+
 Route::get('/', function() {
     return view('home');
 });
@@ -69,8 +83,39 @@ Route::get('/{program}', function($program) {
 
 Route::get('/{program}/contributors', function ($program) {
     // Convert program name into a formatted title
-    $programTitleWords = explode("_", $program);
+    $programTitleWords = explode("-", $program);
     $formattedString = ucwords(join(" ",$programTitleWords));
 
-    return view('contributors', ['message' => "$formattedString Collaborators"]);
+    $testData = [
+        'program' => $formattedString,
+        'departments' => DepartmentTypes::cases(),
+        'contributors' => [
+            [
+                'name' => 'Caleigh Adams',
+                'department' => DepartmentTypes::Performance,
+                'role' => 'Aimee - Part 2',
+                'bio' => "Born and raised in the GTA, Caleigh is a queer actor, writer, and theatre creator, proud to be graduating from the Theatre Performance Program in 2021. Trained in devised and physical theatre, she is drawn to socially engaged projects prioritizing historically underrepresented voices. After spending two years in York University's Film Production BFA, she’s happy to combine her film production knowledge and enthusiasm with her love of theatre. Caleigh has a myriad of interests including expanding her movement horizons, language, and the outdoors. She is thrilled to be working alongside her Humber ensemble and director Christopher Stanton in The Humans."
+            ],
+            [
+                'name' => 'Maddie Bautista',
+                'department' => DepartmentTypes::GuestArtist,
+                'role' => 'Sound Designer',
+                'bio' => "Maddie Bautista is a Bi, Saudi Arabia-born Filipinx sound designer and composer. Maddie has been nominated for a Dora Award for her music in Eraser at RISER 2019, and also nominated for the 2020 Pauline McGibbon Award. Selected sound credits: OIL (ARC), bug (Theatre Passe Muraille), Wah Wah Wah (Wildside 2020), Private Eyes (lemonTree creations), Our Fathers, Sons, Lovers, and Little Brothers (b current). She is Co-Artistic Producer of Bad Muse Collective with Korean-Canadian sound designer Deanna H. Choi. Bad Muse is currently in residence with fu-GEN Theatre to create Love You Wrong Time. www.maddiebautista.com"
+            ],
+            [
+                'name' => 'Ethan Belaisis',
+                'department' => DepartmentTypes::Production,
+                'role' => 'Paint',
+                'bio' => "Hello! My name is Ethan Belaisis, I am a 2nd year in the Theatre Technical Production Course. I enjoy classes such as Props, Scenic Painting, Carpentry as well as Computer Applications. This year has been very different compared to my first year due to the Pandemic and I’ll admit it has been quite the ride. I am currently placed in the Scenic Paint department and I find the work to be quite enjoyable and relaxing most of the time, although some days can prove to be more challenging than others, we will try our best to make sure that we get the work done because the show must go on! There are constant changes because of Covid-19 and we always make sure that we work in a safe sanitized environment and manner as I get to learn how to manage myself in the Paint shop and get the experience that I seek in order to achieve my goals."
+            ],
+            [
+                'name' => 'Eugene Braganza',
+                'department' => DepartmentTypes::Production,
+                'role' => 'Lighting',
+                'bio' => "Eugene Braganza began learning music at the age of four and as a teenager performed in multiple school productions. As an adult he taught music and theatre to children, assisting with school productions of Dr. Seuss’ The Lorax, The Lion King, Aladdin and Matilda – The Musical. At Humber he has worked on the production of Pandora in Bluejeans and The Humans. Eugene wishes to pursue a professional career in the theatre industry as a Technical Director."
+            ]
+        ]
+    ];
+
+    return view('contributors', $testData);
 })->whereIn('program', PROGRAMS);
